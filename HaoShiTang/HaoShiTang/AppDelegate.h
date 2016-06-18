0cbadc549462f2242006dc4cfe0d9ebe212d41c4
zhanglu <lulu@zhangludemacbook-pro.local>
2016-06-18 20:55:39 +0800

    建立项目

---
+++ HaoShiTang/HaoShiTang/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HaoShiTang
+//
+//  Created by zhanglu on 16/6/18.
+//  Copyright © 2016年 zhanglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HaoShiTang/HaoShiTang/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HaoShiTang
+//
+//  Created by zhanglu on 16/6/18.
+//  Copyright © 2016年 zhanglu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
